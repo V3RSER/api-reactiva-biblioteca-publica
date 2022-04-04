5059f36e22bd7a6a7cdcf0691d4e3948cede54c8
V3RSER <luisdeiko@outlook.com>
2022-04-04 03:18:40 -0500

    Conexión MongoDB Atlas

--- src/main/java/co/com/sofka/app/biblioteca/BibliotecaApplication.java
+++ src/main/java/co/com/sofka/app/biblioteca/BibliotecaApplication.java
@@ -2,12 +2,14 @@ package co.com.sofka.app.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class BibliotecaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaApplication.class, args);
+    }
 
 }
